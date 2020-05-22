3703f50820c93b95e6c483e75c3c4521fe00a455
marco <marco.faltoni@outlook.it>
2020-05-22 15:33:51 +0200

    sto facendo ajax

--- main.js
+++ main.js
@@ -3,15 +3,18 @@
 $(document).ready(function() {
 
 
-    $().click(function(){
+    $('.btn-outline-secondary').click(function(){
+
+        var testo_utente = $('#message').val();
+
 
         $.ajax({
             'url':'https://api.themoviedb.org/3/search/movie',
             'method': 'GET',
             'data' : {
-                'api_key': '33f393bb2180fe0fa6a89d6419146443';
-                // 'query': ' ci sarà il valore input dell'utente '
-            }
+                'api_key': '33f393bb2180fe0fa6a89d6419146443',
+                'query': testo_utente
+            },
             'success': function(dati){
                 
             },
