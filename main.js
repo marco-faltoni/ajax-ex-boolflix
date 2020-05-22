243be9ec2b5c36c78aaa70e60e71d0f1cd981aff
marco <marco.faltoni@outlook.it>
2020-05-22 16:00:19 +0200

    stampo

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     $('.btn-outline-secondary').click(function(){
 
         var testo_utente = $('#message').val();
-
+        var recupero_ris;
+        var risultati = {};
 
         $.ajax({
             'url':'https://api.themoviedb.org/3/search/movie',
@@ -16,7 +17,19 @@ $(document).ready(function() {
                 'query': testo_utente
             },
             'success': function(dati){
-                
+                recupero_ris = dati.results;
+                console.log(recupero_ris);
+
+                for (var i = 0; i < recupero_ris.length; i++) {
+                    risultati.titolo = recupero_ris[i].title;
+                    risultati.titolo_originale = recupero_ris[i].original_title;
+                    risultati.lingua = recupero_ris[i].original_language;
+                    risultati.voto = recupero_ris[i].vote_average;
+                    console.log(risultati);
+                    $('.ris').append(risultati);
+                }
+
+
             },
             'error': function() {
                 alert('si è verificato un errore');
@@ -25,6 +38,9 @@ $(document).ready(function() {
 
 
     });
+
+
+
 
 
 
