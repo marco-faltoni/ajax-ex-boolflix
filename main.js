2c7641910dd89375a20dfb092090f9664d822adf
marco <marco.faltoni@outlook.it>
2020-05-22 17:34:46 +0200

    Ottimizzo codice

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ $(document).ready(function() {
 
 
     $('.btn-outline-secondary').click(function(){
-
         var testo_utente = $('#message').val();
-        var recupero_ris;
-
-
+        var recupero_data;
 
         $.ajax({
             'url':'https://api.themoviedb.org/3/search/movie',
@@ -18,25 +15,10 @@ $(document).ready(function() {
                 'query': testo_utente
             },
             'success': function(dati){
-                recupero_ris = dati.results;
-                console.log(recupero_ris);
+                recupero_data = dati.results;
+                console.log(recupero_data);
 
-                for (var i = 0; i < recupero_ris.length; i++) {
-
-                    var risultati = {};
-                    risultati.titolo = recupero_ris[i].title;
-                    risultati.titolo_originale = recupero_ris[i].original_title;
-                    risultati.lingua = recupero_ris[i].original_language;
-                    risultati.voto = recupero_ris[i].vote_average;
-                    console.log(risultati);
-
-                    if (risultati.titolo_originale == risultati.titolo) {
-                        $('.ris').append('<p>'+ risultati.titolo +'</p>', '<p>'+ risultati.lingua +'</p>', '<p>'+ risultati.voto +'</p>');
-                    } else {
-                        $('.ris').append('<p>'+ risultati.titolo +'</p>', '<p>'+ risultati.titolo_originale +'</p>', '<p>'+ risultati.lingua +'</p>', '<p>'+ risultati.voto +'</p>');
-                    }
-
-                }
+                recupero_dati_appendo(recupero_data);
 
             },
             'error': function() {
@@ -46,6 +28,26 @@ $(document).ready(function() {
 
 
     });
+
+    function recupero_dati_appendo(recupero_ris) {
+        for (var i = 0; i < recupero_ris.length; i++) {
+
+            var risultati = {};
+            risultati.titolo = recupero_ris[i].title;
+            risultati.titolo_originale = recupero_ris[i].original_title;
+            risultati.lingua = recupero_ris[i].original_language;
+            risultati.voto = recupero_ris[i].vote_average;
+            console.log(risultati);
+
+            if (risultati.titolo_originale == risultati.titolo) {
+                $('.ris').append('<p>'+ risultati.titolo +'</p>', '<p>'+ risultati.lingua +'</p>', '<p>'+ risultati.voto +'</p>');
+            } else {
+                $('.ris').append('<p>'+ risultati.titolo +'</p>', '<p>'+ risultati.titolo_originale +'</p>', '<p>'+ risultati.lingua +'</p>', '<p>'+ risultati.voto +'</p>');
+            }
+
+        }
+        var testo_utente = $('#message').val('');
+    }
 
 
 
